535e5a8a71e779a58e99d13f49c41bfb4d47f12b
rily <zhangrui_520@live.com>
2018-05-14 01:14:56 +0800

    项目结构

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 var express = require('express');
-var path = require('path')
+var path = require('path');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var serverConfig = require('./commons/config');
 //导入模块路由
 var account = require('./controller/account-controller');
 var app = express();
+
+app.use(session({
+    secret: 'zhxazr',
+    // resave 为强制将session存入store中
+    resave: true,
+    // saveUninitialized为强制将未初始化的session存入store中，这里选择false
+    saveUninitialized: false,
+    cookie: {secure: false, maxAge: 1000 * 60 * 30}
+}));
+// 解析post请求时所传参数
+app.use(bodyParser.json());
 
 //处理静态资源
 app.use(express.static(path.join(__dirname, 'view')));
--- src/controller/account-controller.js
+++ src/controller/account-controller.js
@@ -1,12 +1,30 @@
 var express = require('express');
+var accountService = require('../service/account-service');
 var router = express.Router();
 
 router
-    .get('/register', function (req, resp) {
-        resp.send('注册成功');
+    .post('/register', function (req, resp) {
+        var flag = accountService.accountIsExist(123);
+        // flag = false;
+        if (flag) {
+            resp.send({status: true, msg: '注册成功'});
+        } else {
+            resp.send({status: false, msg: '该账号已存在'});
+        }
     })
-    .get('/login', function (req, resp) {
-        resp.send('登录成功');
+    .post('/login', function (req, resp) {
+        console.log(req.body.password)
+        if (!req.body.account || !req.body.password) {
+            resp.json({status: false, msg: '账户或密码为空'})
+        } else if (!accountService.checkAccount()) {
+            resp.json({status: false, msg: '账户不存在'})
+        } else if (!accountService.checkPassword()) {
+            resp.json({status: false, msg: '密码不正确'})
+        } else {
+            var user = accountService.getUser();
+            req.session.user = user;
+            resp.json({status: true, msg: '登录成功', user: user});
+        }
     });
 
 module.exports = router;
